201657d757b1e0ac188c7dbed907c681dc85ff7b
vraj-patel18 <vrajmd4359@gmail.com>
2026-09-23 21:06:48 +0530

    Adding config file which stores info of database and seat matrices

---
+++ config.py
@@ -0,0 +1,15 @@
+# =========================================
+# MYSQL CONFIGURATION
+# =========================================
+
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "root@123"
+DB_NAME = "flight_reservation"
+
+
+# =========================================
+# SEAT MATRIX FOLDER
+# =========================================
+
+SEAT_FOLDER = "seat_matrices"
